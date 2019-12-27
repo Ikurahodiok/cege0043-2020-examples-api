df4a4c927077e6704ff1781d1d085046c4b27c7a
Claire Ellul <c.ellul@ucl.ac.uk>
2019-12-27 10:38:41 -0500

    added code to query london counties as text

--- routes/geoJSON.js
+++ routes/geoJSON.js
@@ -19,5 +19,22 @@
         res.json({message:req.originalUrl});
     });
 
+    app.get('/postgistest', function (req,res) {
+    pool.connect(function(err,client,done) {
+           if(err){
+               console.log("not able to get connection "+ err);
+               res.status(400).send(err);
+           } 
+           client.query('SELECT name FROM london_counties' ,function(err,result) {
+               done(); 
+               if(err){
+                   console.log(err);
+                   res.status(400).send(err);
+               }
+               res.status(200).send(result.rows);
+           });
+        });
+    });
+
 
     module.exports = geoJSON;
