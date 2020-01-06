cbd4a51837bafc91fa6a4251df94cb1800714f8b
Claire Ellul <c.ellul@ucl.ac.uk>
2020-01-06 10:23:48 -0500

    changed CORS to * - i.e. all sources

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ app.use(function (req, res, next) {
 
 // adding CORS
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "developer.cege.ucl.ac.uk"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
