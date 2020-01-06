f63f1d3ff9e499b4da4dbfcbb057ca7aa62d3bb2
Claire Ellul <c.ellul@ucl.ac.uk>
2020-01-06 11:17:31 -0500

    removed the injection prevention for hte st_geomfromtext as doesn't work and isn't necessary

--- routes/crud.js
+++ routes/crud.js
@@ -38,12 +38,15 @@
       var param6 = "'" +req.body.language+ "'";
       var param7 = "'" +req.body.modulelist+ "'";
      
-      var geometrystring = "st_geomfromtext('POINT($1 $2)')";
+      // no need for injection prevention for st_geomfromtext as if 
+      // the lat/lng values are not numbers it will not process them at all 
+      // impossible to run a statement such as st_geomfromtext('POINT(delete from public.formdata')
+      var geometrystring = "st_geomfromtext('POINT("+req.body.latitude+" "+req.body.longitude"')";
       var querystring = "INSERT into public.formdata(name,surname,module,language, modulelist,location) values ";
       querystring += "($3,$4,$5,$6,$7,";
       querystring += geometrystring + ")";
                 console.log(querystring);
-                client.query( querystring,[param3,param4,param5,param6,param7,param1,param2],function(err,result) {
+                client.query( querystring,[param3,param4,param5,param6,param7],function(err,result) {
                 done();
                 if(err){
                      console.log(err);
